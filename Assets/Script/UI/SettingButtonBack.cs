637b98c239949694bbfa549115c5ff172b7619e9
MaiDucAnh <galuathancongz@gmail.com>
2022-07-14 11:53:22 +0700

    commit design font

--- Assets/Script/UI/SettingButtonBack.cs
+++ Assets/Script/UI/SettingButtonBack.cs
@@ -11,6 +11,7 @@ public class SettingButtonBack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        audioSource.Play();
         button.onClick.AddListener(ClicktoBackMenu);
     }
 
